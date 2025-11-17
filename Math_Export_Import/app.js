e796d3f35bb4f6b9013d552fa1419ff188149918
suyogpagar <167057812+suyogpagar@users.noreply.github.com>
2025-11-17 22:20:05 +0530

    Add files via upload

---
+++ Math_Export_Import/app.js
@@ -0,0 +1,4 @@
+const math = require('./math'); 
+console.log('Add:', math.add(2, 20));         
+
+console.log('Multiply:', math.multiply(2, 20)); 
---
+++ Math_Export_Import/math.js
@@ -0,0 +1,14 @@
+function add(a, b) { 
+  return a + b; 
+} 
+ 
+function multiply(a, b) { 
+  return a * b; 
+} 
+ 
+// Export functions 
+module.exports = { 
+  add, 
+  multiply, 
+}; 
+ 
